4ec5f00dda8705e3e94ebba2860274ebd82e8e9b
ToniRV <tonirosinol@hotmail.com>
2017-03-18 23:05:16 +0100

    Add debuging gui flag to dstar

--- dstar-lite/src/dstar_from_input.cpp
+++ dstar-lite/src/dstar_from_input.cpp
@@ -2,6 +2,145 @@
 
 #include "dstar_lite/dstar.h"
 
+#ifdef MACOS
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
+
+#include <stdlib.h>
+#include <unistd.h>
+#include "dstar_lite/dstar.h"
+
+bool constexpr debug_gui = false;
+
+int hh, ww;
+
+int window;
+Dstar *dstar;
+
+int scale = 30;
+int mbutton = 0;
+int mstate = 0;
+
+bool b_autoreplan = true;
+
+void InitGL(int Width, int Height)
+{
+  hh = Height;
+  ww = Width;
+
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glClearDepth(1.0);
+
+  glViewport(0,0,Width,Height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0,Width,0,Height,-100,100);
+  glMatrixMode(GL_MODELVIEW);
+
+}
+
+void ReSizeGLScene(int Width, int Height)
+{
+  hh = Height;
+  ww = Width;
+
+  glViewport(0,0,Width,Height);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glOrtho(0,Width,0,Height,-100,100);
+  glMatrixMode(GL_MODELVIEW);
+
+}
+
+void DrawGLScene()
+{
+
+  usleep(100);
+
+  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+  glLoadIdentity();
+  glPushMatrix();
+
+  glScaled(scale,scale,1);
+
+  if (b_autoreplan) dstar->replan();
+
+  dstar->draw();
+
+  glPopMatrix();
+  glutSwapBuffers();
+
+}
+
+void keyPressed(unsigned char key, int x, int y)
+{
+  usleep(100);
+
+  switch(key) {
+  case 'q':
+  case 'Q':
+    glutDestroyWindow(window);
+    exit(0);
+    break;
+  case 'r':
+  case 'R':
+    dstar->replan();
+    break;
+  case 'a':
+  case 'A':
+    b_autoreplan = !b_autoreplan;
+    break;
+  case 'c':
+  case 'C':
+    dstar->init(40,50,140, 90);
+    break;
+  }
+}
+
+void mouseFunc(int button, int state, int x, int y) {
+
+  y = hh -y+scale/2;
+  x += scale/2;
+
+  mbutton = button;
+
+  if ((mstate = state) == GLUT_DOWN) {
+    if (button == GLUT_LEFT_BUTTON) {
+      dstar->updateCell(x/scale, y/scale, -1);
+    } else if (button == GLUT_RIGHT_BUTTON) {
+      dstar->updateStart(x/scale, y/scale);
+    } else if (button == GLUT_MIDDLE_BUTTON) {
+      dstar->updateGoal(x/scale, y/scale);
+    }
+  }
+}
+
+void mouseMotionFunc(int x, int y)  {
+
+  y = hh -y+scale/2;
+  x += scale/2;
+
+  y /= scale;
+  x /= scale;
+
+  if (mstate == GLUT_DOWN) {
+    if (mbutton == GLUT_LEFT_BUTTON) {
+      dstar->updateCell(x, y, -1);
+    } else if (mbutton == GLUT_RIGHT_BUTTON) {
+      dstar->updateStart(x, y);
+    } else if (mbutton == GLUT_MIDDLE_BUTTON) {
+      dstar->updateGoal(x, y);
+    }
+  }
+
+}
+
 int main(int argc, char **argv) {
   // Check that there are 3 arguments, aka argc == 4.
   if (argc != 4) {
@@ -50,8 +189,9 @@ int main(int argc, char **argv) {
     }
   }
 
-  Dstar* dstar = new Dstar();
-  dstar->init(start_indices[0], start_indices[1], goal_indices[0], goal_indices[1]);
+  dstar = new Dstar();
+  dstar->init(start_indices[0], start_indices[1]
+      , goal_indices[0], goal_indices[1]);
   for (uint i = 0; i < occupancy_grid.size(); i++) {
     for (uint j = 0; j < occupancy_grid.at(i).size(); j++) {
       if (occupancy_grid.at(i).at(j) == 1) {
