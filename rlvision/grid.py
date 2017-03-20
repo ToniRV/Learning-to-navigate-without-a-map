f410755617a26f934b4cd7d7ea4c65d30790e7ec
Yuhuang Hu <duguyue100@gmail.com>
2017-03-20 18:04:17 +0100

    first draft

--- rlvision/grid.py
+++ rlvision/grid.py
@@ -15,7 +15,7 @@ class Grid(object):
     """A centralized class for managing grid."""
 
     def __init__(self, grid_map, value_map, im_size=None,
-                 start_pos=None, is_po=True,
+                 start_pos=None, is_po=True, mask_radius=3,
                  grid_type="one-is-free"):
         """Init function.
 
@@ -34,6 +34,10 @@ class Grid(object):
             the start position (x, y) in a tuple
             if it's None, then will be randomly drawn from a poll of
             available points.
+        is_po : bool
+            if True, then the map is partially observable
+        mask_radius : int
+            the radius of the visible field
         grid_type : string
             "one-is-free"
             "zero-is-free"
@@ -52,6 +56,7 @@ class Grid(object):
 
         # set if it's partially observable
         self.is_po = is_po
+        self.mask_radius = mask_radius
 
         # supply as a 1-D vector
         if grid_map.ndim == 1:
@@ -97,21 +102,27 @@ class Grid(object):
 
         # set start position
         if not self.is_pos_valid(start_pos):
-            self.start_pos = self.rand_start_pos()
+            self.rand_start_pos()
         else:
-            self.start_pos = start_pos
-
-        # set current agent position
-        self.set_curr_pos(self.start_pos)
-
-        # set initial history, a list, ordered
-        self.pos_history = [self.start_pos]
+            self.set_start_pos(start_pos)
 
         # set goal position, based on value map
         self.set_goal_pos()
 
     def is_pos_valid(self, pos):
-        """Check if the position is valid."""
+        """Check if the position is valid.
+
+        Parameters
+        ----------
+        pos : tuple
+            a tuple (x, y) that represent a number
+
+        Returns
+        -------
+        flag : bool
+            true if valid (one empty position)
+            false if invalid
+        """
         assert isinstance(pos, tuple)
 
         if self.grid_map[pos[0], pos[1]] == self.empty_value:
@@ -120,14 +131,28 @@ class Grid(object):
             return False
 
     def rand_start_pos(self):
-        pass
+        """Choose a start position randomly."""
+        free_list = np.where(self.grid_map == self.empty_value)
+        pos_idx = np.random.randint(free_list[0].shape[0])
+        self.set_start_pos((free_list[0][pos_idx], free_list[1][pos_idx]))
 
     def set_start_pos(self, start_pos):
-        """Set the start position."""
+        """Set the start position.
+
+        Parameters
+        ----------
+        start_pos : tuple
+            potential start position (x, y)
+
+        Returns
+        -------
+        The start position will be set if valid
+        """
         if self.is_pos_valid(start_pos):
             self.start_pos = start_pos
-            # clear all the history caches
+            # clear all the history caches TODO
             self.set_curr_pos(start_pos)
+            self.curr_map = self.get_curr_visible_map(self.start_pos)
             self.pos_history = [start_pos]
         else:
             print ("[MESSAGE] WARNING: The position is not valid, nothing"
@@ -135,7 +160,9 @@ class Grid(object):
 
     def set_goal_pos(self):
         """Set goal position based on the value map."""
-        pass
+        goal_list = np.where(self.value_map == self.value_map.max())
+        # assume the first one
+        self.goal_pos = (goal_list[0][0], goal_list[1][0])
 
     def set_curr_pos(self, curr_pos):
         if self.is_pos_valid(curr_pos):
@@ -143,6 +170,113 @@ class Grid(object):
         else:
             print ("[MESSAGE] WARNING: The position is not a vaild point")
 
-    def set_curr_map(self, curr_map):
-        pass
+    def update_curr_map(self, map_update):
+        """Update current map.
 
+        Parameters
+        ----------
+        map_update : numpy.ndarray
+            the update to the current map
+        """
+        utils.accumulate_map(self.curr_map, map_update,
+                             one_is_free=self.one_is_free)
+
+    def get_curr_visible_map(self, pos):
+        """Get current visible field by given a valid position.
+
+        Parameters
+        ----------
+        pos : tuple
+            a valid position (x, y)
+
+        Returns
+        -------
+        curr_vis_map : numpy.ndarray
+            return a partially visible map by given position.
+            Not if it's fully observable,
+            this function will report the entire map
+        """
+        if self.is_pos_valid(pos):
+            if self.is_po:
+                return utils.mask_grid(pos, self.grid_map, self.mask_radius,
+                                       one_is_free=self.one_is_free)
+            else:
+                return self.grid_map
+
+    def update_state(self, pos_update):
+        """Update state by given position.
+
+        This describe the transition between states.
+        Assume we are at one state t, and by given a
+        position, this function will update the
+        state to t+1
+
+        Parameters
+        ----------
+        pos_update : tuple
+            a position (x, y)
+        """
+        if self.is_pos_valid(pos_update):
+            # append to the history
+            self.pos_history.append(pos_update)
+            # update the current position
+            self.set_curr_pos(pos_update)
+            # update current map
+            self.update_curr_map(self.get_curr_visible_map(pos_update))
+        else:
+            print ("[MESSAGE] WARNING: The position is not valid, nothing"
+                   " is updated")
+
+    def update_state_from_action(self, action):
+        """Update state from action space.
+
+        0 1 2
+        3   4
+        5 6 7
+
+        Update state from action space as above.
+
+        Parameters
+        ----------
+        action : int
+            sample from 0 - 7
+        """
+        pos_update = self.curr_pos
+        if action in [0, 1, 2]:
+            pos_update[0] -= 1
+        elif action in [5, 6, 7]:
+            pos_update[0] += 1
+
+        if action in [0, 3, 5]:
+            pos_update[1] -= 1
+        elif action in [2, 4, 7]:
+            pos_update[1] += 1
+
+        self.update_state(pos_update)
+
+    def get_time(self):
+        """Get the number of states."""
+        return len(self.pos_history)
+
+    def get_state_reward(self):
+        """Return reward for the state.
+
+        Returns
+        -------
+        reward : int
+            return reward for the state
+        state : int
+            1 : success
+           -1 : fail
+            0 : continue
+        """
+        recent_pos = self.pos_history[-1]
+        if recent_pos == self.goal_pos and \
+           self.get_time() <= self.im_size[0]+self.im_size[1]:
+            # success
+            return self.value_map[recent_pos[0], recent_pos[1]], 1
+        elif self.get_time() > self.im_size[0]+self.im_size[1]:
+            # failed
+            return -self.value_map[self.goal_pos[0], self.goal_pos[1]], -1
+        else:
+            return self.value_map[recent_pos[0], recent_pos[1]], 0
