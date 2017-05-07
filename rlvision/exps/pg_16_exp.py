74977583d1323fc0ca70e4221f2864577f688a34
Yuhuang Hu <duguyue100@gmail.com>
2017-05-07 22:22:33 +0200

    update

--- rlvision/exps/pg_16_exp.py
+++ rlvision/exps/pg_16_exp.py
@@ -74,7 +74,7 @@ if network_type == "conv":
 # print model
 model.summary()
 
-model.compile(loss="categorical_crossentropy", optimizer="adam")
+model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 if resume is True:
     model.load_weights(model_path)
 print ("[MESSAGE] Model built.")
