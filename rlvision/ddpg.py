f0d8b2dd54cf65b1dbe850824a9463a0dd4e214b
Yuhuang Hu <duguyue100@gmail.com>
2017-05-15 02:27:24 +0200

    update ddpg

--- rlvision/ddpg.py
+++ rlvision/ddpg.py
@@ -4,11 +4,14 @@ Author: Yuhuang Hu
 Email : duguyue100@gmail.com
 """
 from __future__ import print_function
+from collections import deque
+import random
+import numpy as np
 
 from keras.models import Model
 from keras.layers import InputLayer
 from keras.layers import Dense
-from keras.layers import multiply, add
+from keras.layers import add
 
 import tensorflow as tf
 import keras.backend as K
@@ -129,3 +132,41 @@ class CriticNet(object):
         model.compile(loss='categorical_crossentropy',
                       optimizer="adam")
         return model, A, S
+
+
+class ReplayBuffer(object):
+    """Reply Buffer."""
+    def __init__(self, buffer_size):
+        self.buffer_size = buffer_size
+        self.num_experiences = 0
+        self.buffer = deque()
+
+    def get_batch(self, batch_size):
+        if self.num_experiences < batch_size:
+            return random.sample(self.buffer, self.num_experiences)
+        else:
+            return random.sample(self.buffer, batch_size)
+
+    def size(self):
+        return self.buffer_size
+
+    def add(self, state, action, reward, new_state, done):
+        experience = (state, action, reward, new_state, done)
+        if self.num_experiences < self.buffer_size:
+            self.buffer.append(experience)
+            self.num_experiences += 1
+        else:
+            self.buffer.popleft()
+            self.buffer.append(experience)
+
+    def count(self):
+        return self.num_experiences
+
+    def erase(self):
+        self.buffer = deque()
+        self.num_experiences = 0
+
+
+class OU(object):
+    def function(self, x, mu, theta, sigma):
+        return theta*(mu-x)+sigma*np.random.randn(1)
--- rlvision/exps/ddpg_16_exp.py
+++ rlvision/exps/ddpg_16_exp.py
@@ -6,9 +6,12 @@ Email : duguyue100@gmail.com
 from __future__ import print_function
 import os
 import numpy as np
+import tensorflow as tf
+import keras.backend as K
 
 import rlvision
 from rlvision import grid
+from rlvision.ddpg import ActorNet, CriticNet, ReplayBuffer
 
 
 # load data
@@ -16,3 +19,101 @@ data, value, start_tot, traj_tot, goal_tot, imsize = grid.load_train_grid16()
 data = np.asarray(data, dtype="float32")
 value = np.asarray(value, dtype="float32")
 print ("[MESSAGE] Data Loaded.")
+
+# global parameter
+buffer_size = 100000
+batch_size = 32
+gamma = 0.99
+tau = 0.001
+lra = 0.0001  # learning rate to actor
+lrc = 0.001  # learning rate to critic
+
+action_dim = 8
+state_dim = imsize[0]*imsize[1]
+
+vision = False
+
+explore = 10000.
+episode_count = 2000
+max_steps = 100000
+reward = 0
+done = False
+step = 0
+epsilon = 1
+indicator = 0
+train_indictor = 1  # if 1 then train, 0 then test
+
+config = tf.ConvifProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+K.set_sessions(sess)
+
+actor = ActorNet(sess, state_dim, action_dim, batch_size, tau, lra)
+critic = CriticNet(sess, state_dim, action_dim, batch_size, tau, lrc)
+buff = ReplayBuffer(buffer_size)
+
+
+for game_idx in xrange(episode_count):
+    print("Episode : " + str(game_idx) +
+          " Replay Buffer " + str(buff.count()))
+    for start_pos in [start_tot[game_idx][0]]:
+        # start game
+        game = grid.Grid(data[game_idx], value[game_idx], imsize,
+                         start_pos, is_po=False)
+
+        s_t = game.get_state()
+
+        total_reward = 0.
+        while True:
+            loss = 0
+            epsilon -= 1.0/explore
+            # TODO
+            a_t = actor.model.predict(s_t.reshape(1, s_t.shape[0]))
+
+            # TODO
+            game.update_state_from_action(a_t)
+            r_t, s_t1 = game.get_state_reward()
+
+            # do batch update
+            batch = buff.get_batch(batch_size)
+            states = np.asarray([e[0] for e in batch])
+            actions = np.asarray([e[1] for e in batch])
+            rewards = np.asarray([e[2] for e in batch])
+            new_states = np.asarray([e[3] for e in batch])
+            dones = np.asarray([e[4] for e in batch])
+            y_t = np.asarray([e[1] for e in batch])
+
+            target_q_values = critic.target_model.predict(
+                [new_states, actor.target_model.predict(new_states)])
+
+            for k in xrange(len(batch)):
+                if dones[k]:
+                    y_t[k] = rewards[k]
+                else:
+                    y_t[k] = rewards[k] + gamma*target_q_values[k]
+
+            if (train_indictor):
+                loss += critic.model.train_on_batch([states, actions], y_t)
+                a_for_grad = actor.model.predict(states)
+                grads = critic.gradients(states, a_for_grad)
+                actor.train(states, grads)
+                actor.target_train()
+                critic.target_train()
+
+            total_reward += r_t
+            s_t = s_t1
+
+            print("Episode", game_idx, "Step", step, "Action",
+                  a_t, "Reward", r_t, "Loss", loss)
+
+            step += 1
+            if done:
+                break
+        if (train_indictor):
+            print ("Save model")
+
+        print("TOTAL REWARD @ " + str(game_idx) + "-th Episode  : Reward " +
+              str(total_reward))
+        print("Total Step: " + str(step))
+        print("")
+print ("Finish.")
