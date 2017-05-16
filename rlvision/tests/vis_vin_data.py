46a244f2eb34c58bcaffc3d32ad5d840ddb27f4e
Yuhuang Hu <duguyue100@gmail.com>
2017-05-16 17:11:26 +0200

    update vis vin data

--- rlvision/tests/vis_vin_data.py
+++ rlvision/tests/vis_vin_data.py
@@ -9,17 +9,25 @@ import rlvision
 from rlvision import utils
 from rlvision.utils import process_map_data
 
-#  import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 file_name = os.path.join(rlvision.RLVISION_DATA,
                          "chain_data", "grid16.pkl")
 
 train, test = process_map_data(file_name)
 
+grid = 1-train[0][0, 0]
 
-for idx in xrange(100):
-    #  pos = [train[1][idx]]
-    pos = [(train[1][idx][1], train[1][idx][0])]
-    grid = 1-train[0][idx, 0]
-    utils.plot_grid(grid, (16, 16),
-                    start=None, pos=pos, goal=None, title=None)
+masked_grid = utils.mask_grid((0, 0), grid, 6, one_is_free=True)
+
+plt.figure()
+plt.imshow(masked_grid, cmap="gray")
+
+plt.show()
+
+#  for idx in xrange(100):
+#      #  pos = [train[1][idx]]
+#      pos = [(train[1][idx][1], train[1][idx][0])]
+#      grid = 1-train[0][idx, 0]
+#      utils.plot_grid(grid, (16, 16),
+#                      start=None, pos=pos, goal=None, title=None)
