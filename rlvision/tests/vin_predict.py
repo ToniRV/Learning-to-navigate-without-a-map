1c5f8053c43b5e99b0039106cb84021866974139
Yuhuang Hu <duguyue100@gmail.com>
2017-05-16 15:47:12 +0200

    update vin predict

---
+++ rlvision/tests/vin_predict.py
@@ -0,0 +1,92 @@
+"""VIN predict."""
+
+import os
+import numpy as np
+import cv2
+import keras.backend as K
+
+import rlvision
+from rlvision import utils
+from rlvision.vin import vin_model, get_layer_output
+from rlvision.utils import process_map_data
+
+
+def get_action(a):
+    if a == 0:
+        return -1, -1
+    if a == 1:
+        return 0, -1
+    if a == 2:
+        return 1, -1
+    if a == 3:
+        return -1,  0
+    if a == 4:
+        return 1,  0
+    if a == 5:
+        return -1,  1
+    if a == 6:
+        return 0,  1
+    if a == 7:
+        return 1,  1
+    return None
+
+
+def find_goal(m):
+    return np.argwhere(m.max() == m)[0][::-1]
+
+
+def predict(im, pos, model, k):
+    im_ary = np.array([im]).transpose((0, 2, 3, 1)) \
+        if K.image_dim_ordering() == 'tf' else np.array([im])
+    res = model.predict([im_ary,
+                         np.array([pos])])
+
+    action = np.argmax(res)
+    reward = get_layer_output(model, 'reward', im_ary)
+    value = get_layer_output(model, 'value{}'.format(k), im_ary)
+    reward = np.reshape(reward, im.shape[1:])
+    value = np.reshape(value, im.shape[1:])
+
+    return action, reward, value
+
+
+# load data
+db, im_size = utils.load_grid16(split=1)
+model_file = os.path.join(rlvision.RLVISION_MODEL, "vin_model.h5")
+
+k = 20
+train, test = process_map_data(db, im_size)
+model = vin_model(l_s=test[0].shape[2], k=k)
+model.load_weights(model_file)
+
+for d in zip(*test):
+    im = d[0]
+    pos = d[1]
+    action, reward, value = predict(im, pos, model, k)
+
+    path = [tuple(pos)]
+    for _ in range(30):
+        if im[1][pos[1], pos[0]] == 1:
+            break
+        action, _, _ = predict(im, pos, model, k)
+        dx, dy = get_action(action)
+        pos[0] = pos[0] + dx
+        pos[1] = pos[1] + dy
+        path.append(tuple(pos))
+
+    test_img = cv2.cvtColor(im[0], cv2.COLOR_GRAY2BGR)
+    goal = find_goal(im[1])
+
+    for s in path:
+        cv2.rectangle(test_img, (s[0], s[1]), (s[0], s[1]), (1, 0, 0), -1)
+    cv2.rectangle(test_img, (path[0][0], path[0][1]),
+                  (path[0][0], path[0][1]), (0, 1, 1), -1)
+    cv2.rectangle(test_img, (goal[0], goal[1]),
+                  (goal[0], goal[1]), (0, 0, 1), -1)
+    cv2.imshow("image", cv2.resize(255 - test_img * 255,
+               (300, 300), interpolation=cv2.INTER_NEAREST))
+    cv2.imshow("reward", cv2.resize(reward, (300, 300),
+                                    interpolation=cv2.INTER_NEAREST))
+    cv2.imshow("value", cv2.resize(value / 80, (300, 300),
+               interpolation=cv2.INTER_NEAREST))
+    cv2.waitKey(0)
--- rlvision/utils.py
+++ rlvision/utils.py
@@ -21,6 +21,36 @@ data_dict = ['batch_im_data', 'value_data', 'state_onehot_data',
              'label_data', 'state_y_data', 'im_data', 'state_x_data']
 
 
+def process_map_data(db, im_size):
+    """Pass a db and return train and test for vin."""
+    # split data
+    im_data = db['im_data']
+    value_data = db['value_data']
+    state_data = db['state_xy_data']
+    label_data = db['label_data']
+
+    im_data = np.reshape(im_data, (im_data.shape[0], im_size[0], im_size[1]))
+    value_data = np.reshape(value_data,
+                            (value_data.shape[0], im_size[0], im_size[1]))
+    label_data = np.array([np.eye(1, 8, l)[0] for l in label_data[:, 0]])
+
+    num = im_data.shape[0]
+    num_train = num - num / 5
+    im_train = np.concatenate((np.expand_dims(im_data[:num_train], 1),
+                               np.expand_dims(value_data[:num_train], 1)),
+                              axis=1).astype(dtype=np.float32)
+    state_train = state_data[:num_train]
+    label_train = label_data[:num_train]
+
+    im_test = np.concatenate((np.expand_dims(im_data[num_train:], 1),
+                              np.expand_dims(value_data[num_train:], 1)),
+                             axis=1).astype(dtype=np.float32)
+    state_test = state_data[num_train:]
+    label_test = label_data[num_train:]
+    return (im_train, state_train, label_train), \
+           (im_test, state_test, label_test)
+
+
 def mask_grid(pos, grid, radius, one_is_free=True):
     """Mask a grid.
 
